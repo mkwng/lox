9de83c54a2c5da744dd4b06d83585f835706379e
Michael Wang <hi@michael.wang>
2021-08-29 05:52:16 +0000

    fix bug

--- Scanner.java
+++ Scanner.java
@@ -171,7 +171,7 @@ class Scanner {
     private boolean isAlpha(char c) {
         return (c >= 'a' && c <= 'z') ||
                (c >= 'A' && c <= 'Z') ||
-                c == "_";
+                c == '_';
     }
 
     private boolean isAlphaNumeric(char c) {
